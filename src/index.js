6ae04d7002a52e8fc2592b2aa93cbc250bc083fc
Matt McKnett <mmcknett@gmail.com>
2017-08-29 23:55:25 -0700

    Lift state out of the Square and into the Board.

--- src/index.js
+++ src/index.js
@@ -3,20 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-        value: null,
-    };
-  }
-
   render() {
     return (
       <button
         className="square"
-        onClick={() => this.setState({value: 'X'})}
+        onClick={() => this.props.onClick())}
       >
-        {this.state.value}
+        {this.props.value}
       </button>
     );
   }
@@ -32,7 +25,12 @@ class Board extends React.Component
   }
 
   renderSquare(i) {
-    return <Square value={this.state.squares[i]} />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
